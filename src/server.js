4726f1c583080d3153f21f35273475ade5916d69
choya <gontry2@naver.com>
2019-12-12 22:14:34 +0900

    #2 Introduction to Socket.io

--- src/server.js
+++ src/server.js
@@ -1,16 +1,29 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views")); //view directory 설정
+app.use(logger("dev"));
 app.get("/", (req, res) => res.render("home")); //view는 하나, 다 JS로 짤꺼니깐
 app.use(express.static(join(__dirname, "static"))); // front
-
 const handleListening = () => console.log(`▶ Server running: http://localhost:${PORT}`);
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server); // ws 와 http는 동일한 port에 올릴 수 있음.
+
+io.on("connection", socket => {
+    socket.on("newMessage", ({ message }) => {
+        socket.broadcast.emit("messageNotif", {
+            message,
+            nickname: socket.nickname || "Anon"
+        });
+    });
+    socket.on("setNickname", ({ nickname }) => {
+        socket.nickname = nickname;
+    });
+});
